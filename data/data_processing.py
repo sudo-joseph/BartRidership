33432e7064776f3c5656e8b708f30685ccfd6dde
Joseph Reid <joseph.james.reid@gmail.com>
2020-01-19 11:55:13 -0800

    Fixed int64 not seriable to json bug. Lint.

--- data/data_processing.py
+++ data/data_processing.py
@@ -1,16 +1,13 @@
 """Parse Bart Hourly Ridership Data and save to JSON."""
 import pandas as pd
 # import glob
-from datetime import datetime, timedelta
-from collections import OrderedDict
 
-#BuisnessDay Calculation from here https://stackoverflow.com/a/33480745/12000941
+# BuisnessDay Calculation from here
+# https://stackoverflow.com/a/33480745/12000941
 from pandas.tseries.holiday import USFederalHolidayCalendar
 from pandas.tseries.offsets import CustomBusinessDay
-import pprint
 import copy
 import json
-import math
 
 us_bd = CustomBusinessDay(calendar=USFederalHolidayCalendar())
 colnames = ['date', 'hour', 'orig', 'dest', 'count']
@@ -44,13 +41,15 @@ for month in months:
     output[year] = copy.deepcopy(months)
     output[year][month] = {station: {'arriving':None, 'departing': None} for station in stations}
 
-    #Generate weekday + weekend dataframes for current month
+    # Generate weekday + weekend dataframes for current month
     start = pd.Timestamp('{}-{}'.format(year, months[month])).date()
-    end = (pd.Timestamp('{}-{}'.format(year, months[month])) + pd.offsets.MonthEnd(0)).date()
-    daysOfMonth = pd.date_range(start,end,freq='D')
-    weekdays = pd.bdate_range(start,end,freq=us_bd)
+    end = (pd.Timestamp('{}-{}'.format(year, months[month]))
+           + pd.offsets.MonthEnd(0)).date()
+    daysOfMonth = pd.date_range(start, end, freq='D')
+    weekdays = pd.bdate_range(start, end, freq=us_bd)
     data_weekday = data.loc[data['date'].isin(weekdays)]
-    data_weekend = data.loc[(~data['date'].isin(weekdays)) & data['date'].isin(daysOfMonth)]
+    data_weekend = data.loc[(~data['date'].isin(weekdays))
+                            & data['date'].isin(daysOfMonth)]
 
     for station in stations:
 
@@ -58,30 +57,40 @@ for month in months:
         we_average_arival_hourly, we_average_depart_hourly = [], []
         for hour in range(0, 24):
 
-            wd_orig_station_df = data_weekday.loc[(data_weekday['orig'] == station)
-                                            & (data_weekday['hour'] == hour)]
-            wd_dest_station_df = data_weekday.loc[(data_weekday['dest'] == station)
-                                            & (data_weekday['hour'] == hour)]
+            wd_orig_station_df = data_weekday.loc[
+                                            (data_weekday['orig'] == station)
+                                            & (data_weekday['hour'] == hour)
+                                            ]
+            wd_dest_station_df = data_weekday.loc[
+                                            (data_weekday['dest'] == station)
+                                            & (data_weekday['hour'] == hour)
+                                            ]
             weekday_average_arrival = wd_dest_station_df['count'].sum()
             weekday_average_depature = wd_orig_station_df['count'].sum()
-            wd_average_arival_hourly.append(weekday_average_arrival)
-            wd_average_depart_hourly.append(weekday_average_depature)
+            wd_average_arival_hourly.append(int(weekday_average_arrival))
+            wd_average_depart_hourly.append(int(weekday_average_depature))
 
 
-            we_orig_station_df = data_weekday.loc[(data_weekday['orig'] == station)
-                                            & (data_weekday['hour'] == hour)]
-            we_dest_station_df = data_weekday.loc[(data_weekday['dest'] == station)
-                                            & (data_weekday['hour'] == hour)]
+            we_orig_station_df = data_weekday.loc[
+                                            (data_weekday['orig'] == station)
+                                            & (data_weekday['hour'] == hour)
+                                            ]
+            we_dest_station_df = data_weekday.loc[
+                                            (data_weekday['dest'] == station)
+                                            & (data_weekday['hour'] == hour)
+                                            ]
 
-            we_average_arival_hourly.append(we_dest_station_df['count'].sum())
-            we_average_depart_hourly.append(we_orig_station_df['count'].sum())
+            we_average_arival_hourly.append(
+                                    int(we_dest_station_df['count'].sum()))
+            we_average_depart_hourly.append(
+                                    int(we_orig_station_df['count'].sum()))
             print('Calculating Year: {}, Month: {}, Station: {}, Hour: {}'.format(year,month,station,hour))
 
         output[year][month][station]['arriving'] = {'weekday': wd_average_arival_hourly,
                                                     'weekend': we_average_arival_hourly}
         output[year][month][station]['departing'] = {'weekday': wd_average_depart_hourly,
                                                     'weekend': we_average_depart_hourly}
-    break
+
 
 with open('data/ridership_data.json', 'w') as outfile:
     json.dump(output, outfile)
