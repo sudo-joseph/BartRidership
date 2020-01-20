d4a8f951d3033258ada7671c4eb2db035b81713d
Joseph Reid <joseph.james.reid@gmail.com>
2020-01-19 16:31:13 -0800

    Working render function with default data. preliminary getGraphData function. station name is updating in title.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+let years = ["2019"];
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 ];
@@ -54,32 +55,32 @@ let stations = {
   "MLPT": "Milpitas",
   "PCTR": "Pittsburg Center"};
 
-let ridership_data = {};
-
-// todo clean this up
-// fetch("./data/ridership_data.json")
-// .then(response => response.json())
-// .then(data => {
-// console.log("Got the data!");
-// ridership_data=data;
-// // TODO: Call a function to do something with this data.
-// printData()
-// })
-//
-// function printData() {
-//   console.log(ridership_data);
-// }
+let data = {
+    "ridership_data": {},
+    "arrival_data": [1173, 19, 0, 0, 0, 4579, 17649, 43350, 60828, 37016, 14102, 9107, 8734, 8207, 8442, 9884, 14301, 22133, 18904, 11124, 6956, 5003, 4668, 2209],
+    "depart_data": [617, 165, 0, 0, 2, 1127, 4539, 11820, 20924, 18142, 10802, 9045, 10347, 10063, 11276, 19119, 39175, 65068, 39093, 15905, 6551, 4026, 2955, 1716],
+    "station": stations['12TH']};
+//update with empty values after render function is working.
 
 init();
 
 function init() {
+  fetchData();
   addMonths();
   addStation();
   addListeners();
+  getGraphData()
+}
+
+function fetchData() {
+  fetch("./data/ridership_data.json")
+  .then(response => response.json())
+  .then(json_data => {
+    data.ridership_data=json_data;
+  })
 }
 
 function addMonths() {
-  console.log('addMonths')
   let dropdown = document.querySelector(".Content-Dataview-Controls-Month");
   for (month of months) {
     let option = document.createElement("option");
@@ -91,9 +92,7 @@ function addMonths() {
 }
 
 function addStation() {
-  console.log('addStation')
   let dropdown = document.querySelector(".Content-Dataview-Controls-Station");
-  console.log(stations)
   for (const [stnabr, station] of Object.entries(stations)) {
     let option = document.createElement("option");
     option.value = stnabr;
@@ -106,17 +105,42 @@ function addStation() {
 function addListeners() {
   let controls = document.querySelector(".Content-Dataview-Controls")
   for (child of controls.children) {
-    console.log(child)
-      child.addEventListener("change", hello);
+      child.addEventListener("change", getGraphData);
   }
 }
 
-function hello() {
-  console.log('hello')
+function getGraphData() {
+  let station = document.querySelector(".Content-Dataview-Controls-Station");
+  data.station = stations[station.value];
+  let year = document.querySelector(".Content-Dataview-Controls-Year");
+  let month = document.querySelector(".Content-Dataview-Controls-Month");
+  let dayofWeek = document.querySelector(".Content-Dataview-Controls-DayofWeek");
+  console.log('something')
+  console.log(data.ridership_data)
+
+  data.arrival_data = []
+  data.depart_data = []
+  render()
 }
 
 function render() {
-  //pass
+  let arrival = document.querySelector(".Content-Dataview-Top")
+  let depart = document.querySelector(".Content-Dataview-Bottom")
+  let title = document.querySelector(".Content-Title")
+  // for (hour of data.arrival_data) {
+  //   let div = document.createElement("div");
+  //   div.className += 'Content-Dataview-Data-Box';
+  //   div.style.setProperty('height', hour + 'px')
+  //   arrival.appendChild(div)
+  // }
+  // for (hour of data.depart_data) {
+  //   let div = document.createElement("div");
+  //   div.className += 'Content-Dataview-Data-Box';
+  //   div.style.setProperty('height', hour + 'px')
+  //   depart.appendChild(div)
+  // }
+  title.innerHTML = `<h3> ${data.station} Station Arriving and Departing Passengers</h3>
+                       <h4> (Total arrivals per hour)</h4>`
 }
 
 
